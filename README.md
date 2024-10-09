02fa968ca8101ea2e8826886027ff17951a3ac6d
Peony0019 <1964783912@qq.com>
2024-10-09 13:24:02 +0800

    Complished MLFB_HW1 on 20241008 Wrote part of the FF tools including annuity

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # AquaIsLearning
-# AquaIsLearning
+
+## MLFB - Machine Learning Foundations and Basics
+
+This is a project to records what I'm learning in MLFB course. Codes in python can be found in ./pythonfile, in rust can be found in ./src.
+
+Each session is named by count.
+
+Later on this project will be renamed to MLFB.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
-use plotpy;
-use polars;
+use plotpy::{self, linspace, Curve};
+use polars::prelude::*;
+use rand::prelude::*;
 
 fn main() {
-    println!("{}", polars::frame::DataFrame::empty());
+    println!("{}", DataFrame::empty());
 }
+
+fn series2graph(series: &Series) -> plotpy::GraphMaker {
+    let n = series.len();
+    let x = linspace(0., 1., n);
+    let y = series.into();
+
+    return Curve(&x, &y, "line", "blue");
+}
+
+fn read4web() {
+    // This function reads data from a web API and returns a DataFrame
+    // It's a placeholder and doesn't actually perform the web request
+    let plot: plotpy::Plot = plotpy::Plot::new();
+    let rvs: [f32; 32] = rand::thread_rng().gen();
+    let rv_series: Series = Series::new("col".into(), rvs);
+
+    plot.add(&rv_series);
+
+}
+
+
+
+
+
